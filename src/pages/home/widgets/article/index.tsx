03e6648085c4e78ebffdbfe5e79b89831833bbfa
freelaeder <yanglo67@qq.com>
2023-04-28 16:52:08 +0800

    实现文章分类区域的吸顶效果

--- src/pages/home/widgets/article/index.tsx
+++ src/pages/home/widgets/article/index.tsx
@@ -1,13 +1,15 @@
 // 将文章列表拆分成一个单独的组件 (注意: 不要忘了在首页组件中调用该组件)
 // src/pages/home/widgets/article/index.tsx
 import styles from "@styles/home.module.scss";
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import classNames from "classnames";
 import {useLazyRequestArticlesQuery} from "@store/apiSlice/articleApiSlice";
 import {List} from "react-vant";
 import {useTypedDispatch, useTypedSelector} from "@store/index";
 import {saveArticles, selectArticles} from "@store/slices/articleSlice";
 import {date} from "zod";
+import {useOutletContext} from "react-router-dom";
+import {OutletContextType} from "@shared/layout";
 
 export default function Article() {
     const [type, setType] = useState<ArticleType>('recommend')
@@ -17,6 +19,25 @@ export default function Article() {
     const dispatch = useTypedDispatch()
     // 根据文章类型获取文章状态
     const {current, pageSize, articles, hasMore} = useTypedSelector(selectArticles(type));
+    // 获取内容滚动距离
+    const {scrollTop} = useOutletContext<OutletContextType>()
+    // 记录文章分类是否吸顶
+    const [fixed, setFixed] = useState(false)
+    // 用于记录文章分类区域于页面顶部的距离
+    const offsetTop = useRef(0)
+    // 监听内容区域滚动
+    useEffect(()=> {
+        // if(scrollTop > offsetTop.current){
+        //     setFixed(true)
+        // }else {
+        //     setFixed(false)
+        // }
+        // offsetTop 固定407   scrollTop 变化的滚动
+        // 如果内容区域的滚动距离大于了文章分类区域与页面顶部的距离
+        // 设置吸顶, 否则取消吸顶
+        setFixed(scrollTop > offsetTop.current);
+    },[scrollTop])
+    console.log(offsetTop)
     // 用于加载文章列表
     const loadMore = () => {
         // 发送请求获取文章列表
@@ -43,9 +64,22 @@ export default function Article() {
 
     }
 
+
     return (
         <>
-            <div className={styles.category}>
+            {/*
+        由于文章分类区域变成了固定定位, 脱离了文档流, 不再在页面中占据空间,
+        该区域下面的内容会依次向上移动, 用户体验不好, 所以当文章分类区域变成固定定位以后,
+        渲染下面的 div 用于占位, 占据原本文章分类区域的位置, 防止内容突然上串, 影响用户体验
+      */}
+            {fixed ? <div className={styles.filler}></div> : null}
+            <div ref={(element) => {
+                if (element !== null && !fixed) {
+                    offsetTop.current = element.offsetTop;
+                }
+            }} className={classNames(styles.category, {
+                [styles.fixed]: fixed
+            })}>
                 <span onClick={() => {
                     setType('like')
                 }} className={classNames({[styles.active]: type === 'like'})}>关注</span>
--- src/shared/layout/index.tsx
+++ src/shared/layout/index.tsx
@@ -1,16 +1,30 @@
 // src/shared/layout/index.tsx
 import styles from "@styles/layout.module.scss";
-import { Outlet } from "react-router-dom";
+import {Outlet} from "react-router-dom";
 import Tabbar from "@shared/tabbar";
+import React, {useState} from "react";
+
+
+export interface OutletContextType {
+    scrollTop: number
+}
 
 export default function Layout() {
+    // 用于记录滚动位置
+    const [scrollTop, setScrollTop] = useState(0)
+    // 内容区域滚动事件
+    const scrollHandler = (event: React.UIEvent<HTMLDivElement>) => {
+        // 记录滚动的位置
+        setScrollTop(event.currentTarget.scrollTop)
+    }
+
     return (
         <div className={styles.page}>
-            <div className={styles.container}>
-                <Outlet />
+            <div className={styles.container} onScroll={scrollHandler}>
+                <Outlet context={{scrollTop}}/>
             </div>
             <div className={styles.footer}>
-                <Tabbar />
+                <Tabbar/>
             </div>
         </div>
     );
