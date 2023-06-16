a4ad46023262bfe48e5ecf1786f8ba9450b41694
freelaeder <yanglo67@qq.com>
2023-06-16 08:04:33 +0800

    优化delayTime

--- src/hooks/delayTime.ts
+++ src/hooks/delayTime.ts
@@ -1,25 +1,46 @@
-import {useState, useEffect} from 'react';
+// import {useState, useEffect} from 'react';
+
+// // 倒计时 Hook
+// export default function useCountdown(initialTime: number):[number, () => void] {
+//     const [time, setTime] = useState(initialTime);
+
+//     useEffect(() => {
+//         const timer = setInterval(() => {
+//             setTime((prevTime) => prevTime - 1);
+//         }, 1000);
+
+//         if (time === 0) {
+//             console.log(time)
+//             clearInterval(timer);
+//         }
+
+//         return () => clearInterval(timer);
+//     }, [time]);
+
+//     const reset = () => {
+//         setTime(initialTime);
+//     };
+
+//     return [time, reset];
+// }
+
+import { useState, useEffect } from 'react';
 
 // 倒计时 Hook
-export default function useCountdown(initialTime: number):[number, () => void] {
-    const [time, setTime] = useState(initialTime);
+export default function useCountdown(initialTime: number): [number, () => void] {
+  const [time, setTime] = useState(initialTime);
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setTime((prevTime) => prevTime - 1);
-        }, 1000);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime((prevTime) => (prevTime > 0 ? prevTime - 1 : 0));
+    }, 1000);
 
-        if (time === 0) {
-            console.log(time)
-            clearInterval(timer);
-        }
+    return () => clearInterval(timer);
+  }, []);
 
-        return () => clearInterval(timer);
-    }, [time]);
+  const reset = () => {
+    setTime(initialTime);
+  };
 
-    const reset = () => {
-        setTime(initialTime);
-    };
-
-    return [time, reset];
+  return [time, reset];
 }
